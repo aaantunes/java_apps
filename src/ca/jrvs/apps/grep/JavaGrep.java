650147232537fdc8315bab1ddde3c0b50baa6b17
aaantunes <aaantune@ryerson.ca>
2019-06-27 10:57:22 -0400

    Initial Commit

---
+++ src/ca/jrvs/apps/grep/JavaGrep.java
@@ -0,0 +1,70 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public interface JavaGrep {
+
+    /**
+     * Top level search workflow
+     * @throws java.io.IOException
+     */
+    void process() throws IOException;
+
+    /**
+     * Traverse a given directory and return all files
+     * @param rootDir input directory
+     * @return files under the rootDir
+     */
+    List<File> listFiles(String rootDir);
+
+    /**
+     * Read a file and return all the lines
+     *
+     * Explain FileReader, BufferedReader, and character encoding
+     *
+     * @param inputFile file to be read
+     * @return lines
+     * @throws IllegalArgumentException if a given inputFile is not a file
+     */
+    List<String> readLines(File inputFile);
+
+    /**
+     * Check if a line contains the regex pattern (passed by user)
+     * @param line input string
+     * @return true if there is a match
+     */
+    boolean containsPattern(String line);
+
+    /**
+     * Write lines to a file
+     *
+     * Explore: FileOutputStream, OutputStreamWriter, and BufferedWriter
+     *
+     * @param lines matched line
+     * @throws IOException if write failed
+     */
+    void writeToFile(List<String> lines) throws IOException;
+
+    String getRootPath();
+
+    void setRootPath(String rootPath);
+
+    String getRegex();
+
+    void setRegex(String regex);
+
+    String getOutFile();
+
+    void setOutFile(String outFile);
+}
+
+/* To Do:
+*   1. Write a regex to match all files with .csv file extension
+*   2. "" .. with jpg or jpeg ""
+*   3. '' .. to match the IP addr range from 0.0.0.0 to 999.999.999.999
+*       note: leading 0 is allowed (001 == 1
+*   4. "" to match an empty line
+*
+* */
---
+++ src/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,117 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class JavaGrepImp implements JavaGrep{
+
+    @Override
+    public void process() throws IOException {
+        //call listFiles() and store as list<File>
+        //pass fileList to readLines()
+        //call contains pattern for every
+
+    }
+
+    //use lambda/Steam API's
+    @Override
+    public List<File> listFiles(String rootDir) {
+        List<File> fileList = new ArrayList<>();
+        try {
+            Files.walk(Paths.get(getRootPath()))
+                    .filter(Files :: isRegularFile)
+                    //.collect(Collectors.toList())
+                    //add to List
+                    .forEach(System.out :: println); //DEBUGGING
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileList;
+    }
+
+    //use lambda/Steam API's or BufferedReader and FileReader
+    @Override
+    public List<String> readLines(File inputFile) {
+
+        return null;
+    }
+
+    @Override
+    public boolean containsPattern(String line) {
+        boolean flag = false;
+
+        Pattern checkRegex = Pattern.compile(getRegex());
+        Matcher regexMatcher = checkRegex.matcher(line);
+
+        // Cycles through positive matches in line and prints to screen
+        // Checks string isnt empty and trims any whitespace
+        while (regexMatcher.find()){
+            if (regexMatcher.group().length() != 0){
+                flag = true;
+                System.out.println(regexMatcher.group().trim());
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public void writeToFile(List<String> lines) throws IOException {
+
+    }
+
+    @Override
+    public String getRootPath() {
+        return null;
+    }
+
+    @Override
+    public void setRootPath(String rootPath) {
+
+    }
+
+    @Override
+    public String getRegex() {
+        return null;
+    }
+
+    @Override
+    public void setRegex(String regex) {
+
+    }
+
+    @Override
+    public String getOutFile() {
+        return null;
+    }
+
+    @Override
+    public void setOutFile(String outFile) {
+
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("USAGE: regex rootPath outFile");
+        }
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+
+        try {
+            javaGrepImp.process();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
