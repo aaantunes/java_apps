eff2f703b6d4d277be8e2d29125c6a8779450093
aaantunes <aaantune@ryerson.ca>
2019-07-02 14:30:57 +0000

    updated path

--- src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -1,6 +1,8 @@
 package ca.jrvs.apps.grep;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,83 +16,77 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class JavaGrepImp implements JavaGrep {
+    private String regex;
+    private String rootPath;
+    private String outFile;
 
     @Override
     public void process() throws IOException {
-        //call listFiles() and store as list<File>
-        //pass fileList to readLines()
-        //call contains pattern for every
-
+        List<String> matchedLines = new ArrayList<>();
+        for (File file : listFiles(this.getRootPath())){
+            for (String line : readLines(file)){
+                if (containsPattern(line)){
+                    matchedLines.add(line);
+                }
+            }
+        }
+        writeToFile(matchedLines);
     }
 
     //use lambda/Steam API's
     @Override
     public List<File> listFiles(String rootDir) throws IOException{
-//        List<File> fileList = new ArrayList<>();
-            return Files.walk(Paths.get(rootDir))
-                    .filter(Files::isRegularFile)
-                    .map(Path::toFile)
-                    .collect(Collectors.toList());
+        return Files.walk(Paths.get(rootDir))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
     }
 
     //use lambda/Steam API's or BufferedReader and FileReader
     @Override
-    public List<String> readLines(File inputFile) {
-
-        return null;
+    public List<String> readLines(File inputFile) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        return br.lines().collect(Collectors.toList());
     }
 
     @Override
     public boolean containsPattern(String line) {
-        boolean flag = false;
-
-        Pattern checkRegex = Pattern.compile(getRegex());
-        Matcher regexMatcher = checkRegex.matcher(line);
-
-        // Cycles through positive matches in line and prints to screen
-        // Checks string isnt empty and trims any whitespace
-        while (regexMatcher.find()) {
-            if (regexMatcher.group().length() != 0) {
-                flag = true;
-                System.out.println(regexMatcher.group().trim());
-            }
-        }
-        return flag;
+        return line.matches(this.getRegex());
     }
 
     @Override
     public void writeToFile(List<String> lines) throws IOException {
-
+        Files.write(Paths.get(this.getOutFile()),lines);
     }
 
     @Override
     public String getRootPath() {
-        return null;
+        return rootPath;
     }
 
     @Override
     public void setRootPath(String rootPath) {
-
+        this.rootPath = rootPath;
     }
 
     @Override
     public String getRegex() {
-        return null;
+        return regex;
     }
 
     @Override
     public void setRegex(String regex) {
-
+        this.regex = regex;
     }
 
     @Override
     public String getOutFile() {
-        return null;
+        return outFile;
     }
 
     @Override
     public void setOutFile(String outFile) {
-
+        this.outFile = outFile;
     }
 
     public static void main(String[] args) {
